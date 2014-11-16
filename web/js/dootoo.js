13bfe68eb06340c1fa1281c28f93be3fffe46b86
Brian Cody <brian.m.cody@gmail.com>
2014-11-16 00:06:25 -0800

    front end additions

--- web/js/dootoo.js
+++ web/js/dootoo.js
@@ -3,12 +3,14 @@
 ///////////////
 var geocoder;
 var map;
+var distance = 20;
 
 function init_map() { // add parameter for location data
 	geocoder = new google.maps.Geocoder();
 	var mapOptions = {
 			center: { lat: 35.27445, lng: -120.653455 },
-			zoom: 12
+			zoom: 12,
+			maxZoom: 18
 	};
 	map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
@@ -18,6 +20,56 @@ google.maps.event.addDomListener(window, 'load', init_map);
 ///////////////////
 // Rest Handling //
 ///////////////////
+var baseUrl = 'http://localhost:8081/';
+
+///////////////////
+//     Auth      //
+///////////////////
+function check_logged_in() {
+	// call out to backend
+	// if user is logged in, do not display the login dialog
+}
+
+function login() {
+	$('#password').val('');
+	if (!valid_username())
+		return false
+	var url = baseUrl + 'users/search/findById?email=' + $('#username').val();
+	//TODO: call the correct endpoint for shiro stuff
+	$.ajax({
+		url:url,
+		type:'GET',
+		dataType:'json'
+	}).done(function(data) {
+		if (data._embedded != null && data._embedded.users.length > 0) {
+			var user = data._embedded.users[0];
+			$('#login_nav').html('<li class="navbar-text">Hi, ' + user.firstName + '!</li><li><a href="/web" class="navbar-link">logout</a></li>');
+		} else {
+			alert('Could not login!');
+		}
+	}).fail(function() {
+		alert('Could not login');
+	});
+	return true;
+}
+
+function valid_username() {
+	if ($('#username').val() == '' || $('#username').val().indexOf('@') <= 0) {
+		$('#username_group').addClass('has-error');
+		return false;
+	} else {
+		$('#username_group').removeClass('has-error');
+	}
+	
+	return true;
+}
+
+check_logged_in();
+
+///////////////////
+//   filtering   //
+///////////////////
+var tags = [];
 function change_zip() {
 	var where = document.getElementById('loc').value;
 	if (where == null || where == '') {
@@ -27,13 +79,14 @@ function change_zip() {
 		if (status == google.maps.GeocoderStatus.OK) {
 			var lng = results[0].geometry.location.lng();
 			var lat = results[0].geometry.location.lat();
-			var url = 'http://localhost:8081/happenings/search/findByLocNear?loc=' + lng + ',' + lat
+			var url = 'http://localhost:8081/happenings/search/findByLocNear?loc=' + lat + ',' + lng + '&distance=' + distance + 'miles';
 			$.ajax({
 				url:url,
 				type:'GET',
 				dataType:'json'
 			}).done(function(data) {
 				map.setCenter(results[0].geometry.location);
+				map.setZoom(12);
 				$('#happenings').html('');
 				display_happenings(data);
 				$('#loc').val('');
@@ -46,20 +99,23 @@ function change_zip() {
 }
 
 function filter_happenings() {
+	// TODO: keep the existing tags?
 	var what = document.getElementById('tags').value;
+	tags = what.replace(', ',' ').replace(',',' ').split(' ');
 	if (what == null || what == '') {
 		return false;
 	}
 	var latlng = map.getCenter();
 	var lat = latlng.lat();
 	var lng = latlng.lng();
-	var url = 'http://localhost:8081/happenings/search/findByTagsInAndLocNear?tags=' + what + '&loc=' + lng + ',' + lat;
+	var url = 'http://localhost:8081/happenings/search/findByTagsInAndLocNear?tags=' + tags + '&loc=' + lat + ',' + lng + '&distance=' + distance + 'miles';
 	$.ajax({
 		url:url,
 		type:'GET',
 		dataType:'json'
 	}).done(function(data) {
 		$('#happenings').html('');
+		pop_pills(tags);
 		display_happenings(data);
 		$('#tags').val('');
 	});
@@ -68,12 +124,29 @@ function filter_happenings() {
 
 function display_happenings(data) {
 	if (data._embedded != null) {
+		var bounds = new google.maps.LatLngBounds();
 		data._embedded.happenings.forEach(function(hap) {
-			var row = "<tr><td><strong>" + hap.title + "</strong><br>";
+			var row = '<tr><td><strong>' + hap.title + '</strong><br>';
 			row += hap.description + "<br>";
 			row += hap.address + " " + hap.startTime + " - " + hap.endTime;
 			$('#happenings').append(row);
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(hap.loc.y,hap.loc.x),
+				map: map,
+				title: hap.title
+			});
+			bounds.extend(marker.position);
 		});
+		map.fitBounds(bounds);
 	}
-	
+}
+
+function pop_pills(tags) {
+	$('#tag_pills').html('');
+	tags.forEach(function (tag) {
+		//TODO: add onclick to the 'x'
+		var html = '<div class="btn-group btn-group-xs" role="group"><button class="btn btn-primary btn-xs disabled" type="button">' + tag + '</button>';
+		html += '<button class="btn btn-primary btn-xs" type="button"><span aria-hidden="true">&times;</button></div> ';
+		$('#tag_pills').append(html);
+	});
 }
